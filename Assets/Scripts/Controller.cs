501109a7682582d6ae9eb8a914dddf34e58948da
MoonCrowned <loginovpb@mail.ru>
2022-12-05 12:19:58 +0300

    play, frame, lerp key in void

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
@@ -27,27 +28,56 @@ public class Controller : MonoBehaviour
     public Slider timelineSlider;
     public int lerpDistance = 15;
 
+    public TMP_Text frameNumberText;
+    //public int lerpInVoidDistance = 15;
+
     private int currentPhoto;
     public Texture2D currentTexture;
 
     public WeightSlider weightSliderPrefab;
     public List<WeightSlider> weightSliders;
 
+    public Button playButton;
+    public TMP_InputField playFpsInput;
+    public int playFps;
+    private bool isPlaying;
+    private float playingLastFrameTime;
+
 
     private void Awake()
     {
         weightSliderPrefab.gameObject.SetActive(false);
         saveButton.onClick.AddListener(Save);
+        playFpsInput.onEndEdit.AddListener((s) =>
+        {
+            float f = (float)playFps;
+            float.TryParse(s, out f);
+            playFps = (int)f;
+        });
+        playButton.onClick.AddListener(() => OnPlayPressed());
+    }
+
+    private void OnPlayPressed()
+    {
+        if (isPlaying)
+        {
+            isPlaying = false;
+        }
+        else
+        {
+            isPlaying = true;
+            playingLastFrameTime = Time.time;
+        }
     }
 
     async void Save()
     {
         Debug.Log("Saving, wait");
 
-        string allDataPath = Path.Combine(folder, Path.GetFileName(folder) + "_" + DateTime.Now.Ticks+".json");
+        string allDataPath = Path.Combine(folder, Path.GetFileName(folder)+".json");
         File.WriteAllText(allDataPath, JsonUtility.ToJson(photoData));
         
-        float timeStart = Time.time;
+        /*float timeStart = Time.time;
         int numSaved = 0;
         foreach (var data in photoData.photoData)
         {
@@ -60,7 +90,7 @@ public class Controller : MonoBehaviour
                 timeStart = Time.time;
                 Debug.Log((100f*(float)numSaved/(float)photoData.photoData.Count).ToString("0.0")+"%");
             }
-        }
+        }*/
         Debug.Log("Saved");
     }
 
@@ -81,6 +111,22 @@ public class Controller : MonoBehaviour
                 NextPhoto();
             if (Input.GetKeyDown(KeyCode.Alpha1))
                 PreviousPhoto();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnPlayPressed();
+        }
+        
+        if (isPlaying)
+        {
+            while (Time.time - playingLastFrameTime > 1f / (float)playFps)
+            {
+                NextPhoto();
+                if (currentPhoto >= photoData.photoData.Count-1)
+                    isPlaying = false;
+                playingLastFrameTime += 1f / (float)playFps;
+            }
         }
     }
 
@@ -109,6 +155,7 @@ public class Controller : MonoBehaviour
             Destroy(currentTexture);
 
         timelineSlider.value = (float)currentPhoto;
+        frameNumberText.text = "" + currentPhoto;
 
         string photoPath = Path.Combine(folder, photoData.photoData[currentPhoto].photoPath);
         currentTexture = (new Texture2D(1, 1, TextureFormat.RGBA32, false));
@@ -217,45 +264,54 @@ public class Controller : MonoBehaviour
             weightSliders.Add(weightSlider);
         }
         
-        foreach (var photoPath in Directory.EnumerateFiles(folder))
+        string allDataPath = Path.Combine(folder, Path.GetFileName(folder)+".json");
+        if (File.Exists(allDataPath))
         {
-            PhotoData newPhotoData = null;
+            photoData = JsonUtility.FromJson<AllPhotosData>(File.ReadAllText(allDataPath));
+        }
+        else
+        {
 
-            string extention = Path.GetExtension(photoPath);
-            if (!(extention == ".jpg" || extention == ".png" || extention == ".jpeg"))
-                continue;
+            foreach (var photoPath in Directory.EnumerateFiles(folder))
+            {
+                PhotoData newPhotoData = null;
 
-            string dataName = Path.GetFileNameWithoutExtension(photoPath) + ".key";
-            string dataPath = Path.Combine(folder, dataName);
-            if (File.Exists(dataPath))
-            {
-                string dataJson = File.ReadAllText(dataPath);
-                newPhotoData = JsonUtility.FromJson<PhotoData>(dataJson);
-            }
+                string extention = Path.GetExtension(photoPath);
+                if (!(extention == ".jpg" || extention == ".png" || extention == ".jpeg"))
+                    continue;
 
-            if (newPhotoData == null)
-            {
-                newPhotoData = new PhotoData();
-                newPhotoData.photoPath = Path.GetFileName(photoPath);
-                newPhotoData.photoName = Path.GetFileNameWithoutExtension(photoPath);
-                newPhotoData.keyData = new List<KeyData>();
-            }
-            
-            for (int k=0; k<skinnedMeshRenderer.sharedMesh.blendShapeCount; k++ )
-            {
-                string blendshapeName = skinnedMeshRenderer.sharedMesh.GetBlendShapeName(k);
-                if (newPhotoData.keyData.FirstOrDefault(key => key.name == blendshapeName) == null)
+                string dataName = Path.GetFileNameWithoutExtension(photoPath) + ".key";
+                string dataPath = Path.Combine(folder, dataName);
+                if (File.Exists(dataPath))
                 {
-                    var keyData = new KeyData();
-                    keyData.name = blendshapeName;
-                    keyData.key = 0f;
-                    keyData.isKey = false;
-                    newPhotoData.keyData.Add(keyData);
+                    string dataJson = File.ReadAllText(dataPath);
+                    newPhotoData = JsonUtility.FromJson<PhotoData>(dataJson);
                 }
+
+                if (newPhotoData == null)
+                {
+                    newPhotoData = new PhotoData();
+                    newPhotoData.photoPath = Path.GetFileName(photoPath);
+                    newPhotoData.photoName = Path.GetFileNameWithoutExtension(photoPath);
+                    newPhotoData.keyData = new List<KeyData>();
+                }
+
+                for (int k = 0; k < skinnedMeshRenderer.sharedMesh.blendShapeCount; k++)
+                {
+                    string blendshapeName = skinnedMeshRenderer.sharedMesh.GetBlendShapeName(k);
+                    if (newPhotoData.keyData.FirstOrDefault(key => key.name == blendshapeName) == null)
+                    {
+                        var keyData = new KeyData();
+                        keyData.name = blendshapeName;
+                        keyData.key = 0f;
+                        keyData.isKey = false;
+                        newPhotoData.keyData.Add(keyData);
+                    }
+                }
+
+                photoData.photoData.Add(newPhotoData);
+
             }
-            
-            photoData.photoData.Add(newPhotoData);
-            
         }
 
         timelineSlider.minValue = 0;
@@ -273,7 +329,7 @@ public class Controller : MonoBehaviour
     void LerpKeysAroundKey(int keyNum, int lineNum)
     {
         // Smooth all not keys around this key
-        int nearestLeftKey = -1;
+        int nearestLeftKey = keyNum - lerpDistance;
         for (int i = keyNum - 1; i >= keyNum - lerpDistance; i--)
         {
             if (i < 0)
@@ -297,7 +353,7 @@ public class Controller : MonoBehaviour
         }
 
         // Smooth all not keys around this key
-        int nearestRightKey = -1;
+        int nearestRightKey = keyNum + lerpDistance;
         for (int i = keyNum + 1; i <= keyNum + lerpDistance; i++)
         {
             if (i >= photoData.photoData.Count)
